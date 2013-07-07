9f24820785e010ecb9c0b3c1d00aee899dd28fd3
Chris Birchall <chris.birchall@gmail.com>
2013-07-07 21:32:53 +0900

    Prepare for running on Engine Yard

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express')
   , git = require('./git')
   , linecount = require('./linecount');
 
-server.listen(8080);
+server.listen(process.env.PORT || 8080);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
